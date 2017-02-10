35fb9bf4c44d68f789bea5adffd7bd4b679bea37
Tsung-Ting Tzang <ethantzang@nestlabs.com>
2017-02-09 22:48:59 -0800

    Initialize firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAJIFw4UGoaJQkONe1hOGh-DBN04KI2l_s",
+  authDomain: "blinding-torch-957.firebaseapp.com",
+  databaseURL: "https://blinding-torch-957.firebaseio.com",
+  storageBucket: "blinding-torch-957.appspot.com",
+  messagingSenderId: "50701028887"
+}
+
+firebase.initailizeApp(config);
 
 ReactDOM.render(
   <App />,
